f9e9da8dd787ad7224688d7ce86debe2046029b6
Chris Barker <cbarker@puppetlabs.com>
2012-10-11 14:52:49 -0700

    Creates base files used for soup scripts

---
+++ vagrantup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# vagrantup.sh: will run vagrant up against all agents listed
+
+if [ -z "$1" ]
+then
+  echo "Usage: `basename $0` [vagrant-agent1] [vagrant-agent2]"
+  exit $E_NOARGS
+fi
+
+if [ ! -e Vagrantfile ]
+then
+  echo "Have to run in the a Vagrant folder"
+  exit $E_NOARGS
+fi
+
+until [ -z "$1" ]
+do
+  #echo "vagrant up ${1}"
+  vagrant up ${1}
+  shift
+done
